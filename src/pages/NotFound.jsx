9e6a3250d6367d6d3202e14133119821d4ee30ef
Ade Oktaviano Arrahman <adeoktaviano50@gmail.com>
2022-08-06 09:08:07 +0700

    add notfound

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+export default function NotFound(){
+    return(
+        <>
+        <div className="flex justify-center gap-1 flex-col h-screen items-center">
+            <h1>Not Foung 404</h1>
+            <div className="h-5 items-center group w-1/3 flex justify-center">
+                <div className="bg-black h-[1px] w-full flex items-center relative">
+                    <div className="w-3 h-3 bg-slate-300 relative transition ease-out duration-500 group-hover:translate-x-28 translate-x-2 border-[4px]  border-black rounded-full "></div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
